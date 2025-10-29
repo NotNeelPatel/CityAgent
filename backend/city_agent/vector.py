3145a8a2bce9255cc9d19f436836e49a3ef2e91b
Aashna-Verma <aashna.verma@outlook.com>
2025-10-28 23:33:58 -0400

    vector db

---
+++ backend/city_agent/vector.py
@@ -0,0 +1,37 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd  # to read csv
+
+df = pd.read_csv(".city_agent/data/1_Operating_overview_expenditure.csv")
+
+os.environ["OLLAMA_API_BASE"] = os.getenv("OLLAMA_API_BASE")
+embeddings = OllamaEmbeddings(model="embeddinggemma:300m")
+
+db_location = "./chroma_langchain_db"
+add_documents = not os.path.exists(db_location)
+
+if add_documents:
+    documents = []
+    ids = []
+
+    for i, row in df.iterrows():
+        document = Document(
+            page_content=row["Committee"] + " " + row["source de financement"],
+            metadata={"Total": row["Total"]},
+            id=str(i),
+        )
+        ids.append(str(i))
+        documents.append(document)
+
+vector_store = Chroma(
+    collection_name="Operating_overview_expenditure",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+if add_documents:
+    vector_store.add_documents(documents=documents, ids=ids)
+
+retriever = vector_store.as_retriever(search_kwargs={"k": 5})
