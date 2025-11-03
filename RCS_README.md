1929ce133dfb50d2794a0549785a93b3874a659d
NotNeelPatel <91706742+NotNeelPatel@users.noreply.github.com>
2025-11-03 10:39:00 -0500

    #48: Update the main code to comply with all current RCS requirements and provide instructions

---
+++ RCS_README.md
@@ -0,0 +1,26 @@
+# RCS Ollama
+
+More information: https://carleton.ca/rcs/llm-access-instructions/ 
+
+LLM Server: https://rcsllm.carleton.ca/rcsapi
+
+Requirements: 
+- Ensure that you have connected to the VPN
+- Ensure that you have an API key
+
+If you have any more questions, ask Neel
+
+## Configuring/Running the Demo
+
+Create a venv and install the requirement (only requirement is ollama)
+
+```
+# CREATE VENV FIRST
+pip install ollama
+```
+
+Run main.py, passing your API key as an argument
+
+```
+python main.py --api_key <your api key>
+```
--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 from ollama import Client
 from argparse import ArgumentParser
 
-#api_key = "<your_api_key_here>" #or provide via parser argument
-
-api_key = ""
-
-host = 'https://134.117.214.87/rcsapi'
+DEFAULT_HOST = "https://rcsllm.carleton.ca/rcsapi"
 
 parser = ArgumentParser(description="Ollama API Client Example")
-
+parser.add_argument("--host", type=str, help="Ollama server host", required=False, default=DEFAULT_HOST)
 parser.add_argument("--model", type=str, help="Model to use for requests", required=False, default="gpt-oss:120b")
-
+parser.add_argument("--prompt", type=str, help="Prompt to send to the model", required=False, default="What is an LLM and how are you compared to other models?")
 parser.add_argument("--stream", action="store_true", help="Stream the response", required=False, default=True)
-
-parser.add_argument("--api_key", type=str, help="API Key for authentication", required=False, default=api_key)
-
+parser.add_argument("--api_key", type=str, help="API Key for authentication", required=True)
 parser.add_argument("--list_models", action="store_true", help="List available models", required=False, default=False)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
     api_key = args.api_key
     custom_header = {"x-api-key": api_key} if api_key else {}
-    ollama_client = Client(host=host, headers=custom_header, verify=False)
+    ollama_client = Client(host=args.host, headers=custom_header)
+    
     if args.list_models:
         models = ollama_client.list()
         print("Available models:")
         for model in models.models:
             print(f"- {model.model}")
         exit(0)
+
     stream = args.stream
+    
     response = ollama_client.chat(
         model=args.model,
         messages=[
-            {"role": "user", "content": "What is Carleton University?"}
+            {"role": "user", "content": args.prompt}
         ],
         stream=stream
     )
+
     if stream:
         for chunk in response:
             print(chunk['message']['content'], end="", flush=True)
